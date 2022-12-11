a91f10c236243e4f1d21ca216dcda28aea1ed266
Stefano <stdemicheli@protonmail.com>
2022-12-11 06:37:56 +0100

    Implements reading files.

--- files-and-storage/src/main.rs
+++ files-and-storage/src/main.rs
@@ -2,9 +2,43 @@ use bincode::serialize as to_bincode;
 use serde_cbor::to_vec as to_cbor;
 use serde_json::to_string as to_json;
 use serde_derive::{Serialize};
+use std::fs::File;
+use std::io::prelude::*;
+use std::env;
 
 fn main() {
+    read_files();
 }
+
+// Reading files
+
+const BYTES_PER_LINE: usize = 16;
+
+fn read_files() {
+    let arg1 = env::args().nth(1);
+
+    let fname = arg1.expect("usage: fview FILENAME");
+
+    let mut f = File::open(fname).expect("Unable to open file");
+    let mut pos = 0;
+    let mut buffer = [0; BYTES_PER_LINE];
+
+    while let Ok(_) = f.read_exact(&mut buffer) {
+        print!("[0x{:08x}] ", pos);
+        for byte in &buffer {
+            match *byte {
+                0x00 => print!(".  "),
+                0xff => print!("## "),
+                _ => print!("{:02x} ", byte),
+            }
+        }
+
+        println!("");
+        pos += BYTES_PER_LINE;
+    }
+}
+
+// Decoding
 
 #[derive(Serialize)]
 struct City {
@@ -14,7 +48,7 @@ struct City {
     longitude: f64,
 }
 
-fn decodeCities() {
+fn decode_cities() {
     let calabar = City {
         name: String::from("Calabar"),
         population: 470_000,
